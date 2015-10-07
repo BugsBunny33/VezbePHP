10e210a4ead121a1dbc8b440c328071bdbe2bf61
BugsBunny33 <caldovic@gmail.com>
2015-10-07 13:44:54 +0200

    poceh da vezbam php

---
+++ exercises_with_solution_30.php
@@ -0,0 +1,13 @@
+<?php  
+/* 1
+phpinfo(); */
+
+/*2
+echo <<< _END
+Tomorrow I \'ll learn PHP global variables. </br>
+This is a bad command : del c:\\*.* 
+_END; */
+
+/*3*/
+
+?>
---
+++ treci.php
@@ -0,0 +1,15 @@
+
+
+<?php
+$var = 'PHP Tutorial';
+?>
+<!DOCTYPE  html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title><?php echo $var?> - W3 resource! </title>
+  </head>
+  <body>
+  
+  </body>
+</html>
